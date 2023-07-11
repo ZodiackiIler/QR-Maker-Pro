77fcc2867d2dc97cfe3fe959aaf1405049670f5b
Tenebrae <80919466+ZodiackiIler@users.noreply.github.com>
2023-07-11 20:52:09 +0300

    Update qr_code_maker.py

--- qr_code_maker.py
+++ qr_code_maker.py
@@ -1,18 +1,24 @@
+# Импортируем библиотеки
 import qrcode
 from PIL import Image
 import os
 
 def generate_qr_code_with_image(data, image_path, output_path):
+    # Создание объекта QRCode с параметрами QR-кода
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
         box_size=10,
         border=4,
     )
+    # Добавление данных в QR-код
     qr.add_data(data)
+    # Создание QR-кода
     qr.make(fit=True)
 
     # Создаем изображение QR-кода
+    # fill_color - цвет линий qrcode
+    # back_color - задний фон qrcode
     qr_image = qr.make_image(fill_color="black", back_color="white")
 
     # Добавляем изображение в QR-код, если image_path не пусто
@@ -20,7 +26,7 @@ def generate_qr_code_with_image(data, image_path, output_path):
         # Открываем изображение, которое вы хотите добавить в QR-код
         overlay_image = Image.open(image_path)
 
-        # Расчет размера и позиции изображения на QR-коде
+        # Расчитываем размер и позиции изображения на QR-коде
         qr_width, qr_height = qr_image.size
         overlay_width, overlay_height = overlay_image.size
         overlay_size = min(qr_width, qr_height) // 4
@@ -43,9 +49,9 @@ def generate_qr_code_with_image(data, image_path, output_path):
     qr_image.save(output_path)
 
 # Пример использования:
-data = "https://example.com"
+data = "Строка1\nСтрока2\nСтрока3"
 image_path = input("Введите путь к изображению (если нет, нажмите Enter): ")
-output_path = "qr_code_with_image.png"
+output_path = "qr_code.png"
 
 generate_qr_code_with_image(data, image_path, output_path)
-print(f"QR-код сохранен в файл: {output_path}")
+print(f"QR-код был сохранен в файл: {output_path}")
